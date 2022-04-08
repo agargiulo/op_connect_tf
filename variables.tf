c0eeea14b755d85512800a9fda7511d2b2863996
Anthony Gargiulo <code@agarg.me>
2022-04-07 21:55:36 -0700

    Add variable descriptions and UNLICENSE

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "prefix" {
+  description = "Prefix for various resource names"
   default = "op-conn"
 }
 
 variable "op_creds_base64" {
+  description = "Base64 encoded 1password-credentials.json file"
   type = string
 }
 
 variable "op_networking" {
+  description = "Networking items, for ALB and ECS Service"
   type = object({
     vpc_id  = string
     subnets = list(string)
@@ -14,6 +17,7 @@ variable "op_networking" {
 }
 
 variable "op_alb" {
+  description = "Options for the Application Load Balancer"
   type = object({
     inbound_cidr = list(string)
     acm_cert_arn = string
