312dc16c63716c0ccfb3981f04df9c583eca7c39
Anthony Gargiulo <code@agarg.me>
2022-04-07 21:59:39 -0700

    Remove unneeded providers

--- providers.tf
+++ providers.tf
@@ -4,14 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.9"
     }
-    dns = {
-      source  = "hashicorp/dns"
-      version = "~> 3.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
   }
   required_version = "~> 1.1"
 }
